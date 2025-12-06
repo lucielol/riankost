8df144e16a1a8edd4a8a665213c91fdc6df41d39
Luciel <lucielol1337@proton.me>
2025-12-06 17:55:42 +0700

    refactor: Update router imports to use path aliases.

--- apps/server/src/router/index.ts
+++ apps/server/src/router/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
-import authRouter from "./auth";
-import userRouter from "./user";
+import authRouter from "@/router/auth";
+import userRouter from "@/router/user";
 
 const router = new Hono();
 
